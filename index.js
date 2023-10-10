aac3df4ca2d04c1ae4b4273a321a624df26a7e3a
Meow <tabbathapischke@gmail.com>
2023-10-09 14:03:06 -1000

    add question array

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
-// TODO: Include packages needed for this application
+// Includes packages needed for this application
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your project title?',
+      name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What does your project do?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the instructions for installation of your project?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage your project?',
+        name: 'usage',
+    },
+    {
+        type: 'checklist',
+        message: '',
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+    },
+  ])
+  .then(answers => console.log(answers));
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
